1f387ae09bc597a6018a61192155bcb8896d864d
aleksey <permmedium@yandex.ru>
2024-11-12 13:25:41 +0500

    autorization

--- server/internal/auth/handler.go
+++ server/internal/auth/handler.go
@@ -2,8 +2,8 @@ package auth
 
 import (
 	"net/http"
-	"os"
 	"server/configs"
+	"server/pkg/jwt"
 	"server/pkg/logger"
 	"server/pkg/request"
 	"server/pkg/response"
@@ -11,16 +11,20 @@ import (
 
 type AuthHandlerDeps struct {
 	*configs.Config
+	*AuthService
 }
 
 type AuthHandler struct {
 	*configs.Config
+	*AuthService
 }
 
 func NewAuthHandler(router *http.ServeMux, deps AuthHandlerDeps) {
 	handler := &AuthHandler{
-		Config: deps.Config,
+		Config:      deps.Config,
+		AuthService: deps.AuthService,
 	}
+
 	router.HandleFunc("POST /auth/login", handler.login())
 	router.HandleFunc("POST /auth/register", handler.register())
 }
@@ -29,19 +33,34 @@ func (handler *AuthHandler) login() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		logger.Message("login")
 
-		_, err := request.HandleBody[LoginRequest](&w, req)
+		body, err := request.HandleBody[LoginRequest](&w, req)
 
 		if err != nil {
 			logger.Error(err)
-			response.Json(w, err.Error(), 402)
+			http.Error(w, err.Error(), http.StatusBadRequest)
 			return
 		}
 
-		result := LoginResponse{
-			Token: os.Getenv("TOKEN"),
+		email, err := handler.AuthService.Login(body.Email, body.Password)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
 		}
 
-		response.Json(w, result, 200)
+		jwtService := jwt.NewJWT(handler.Auth.Secret)
+		token, err := jwtService.Create(email)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		responseData := LoginResponse{
+			Token: token,
+		}
+
+		response.Json(w, responseData, http.StatusOK)
 	}
 }
 
@@ -49,7 +68,7 @@ func (handler *AuthHandler) register() http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
 		logger.Message("register")
 
-		_, err := request.HandleBody[RegisterRequest](&w, req)
+		body, err := request.HandleBody[RegisterRequest](&w, req)
 
 		if err != nil {
 			logger.Error(err)
@@ -57,6 +76,25 @@ func (handler *AuthHandler) register() http.HandlerFunc {
 			return
 		}
 
-		w.Write([]byte("REGISTER SUCCESS"))
+		email, err := handler.AuthService.Register(body.Email, body.Password, body.Name)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		jwtService := jwt.NewJWT(handler.Config.Auth.Secret)
+		token, err := jwtService.Create(email)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		responseData := &RegisterResponse{
+			Token: token,
+		}
+
+		response.Json(w, responseData, http.StatusOK)
 	}
 }
